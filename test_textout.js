4ecacdaf5bae02f5115468063df0c43217867aee
Ryan Birmingham <birm@rbirm.us>
2017-02-14 16:10:51 -0500

    add tape test and angular

--- test_textout.js
+++ test_textout.js
@@ -1,5 +1,5 @@
 /**
- *  @fileoverview Text lauout test commands.
+ *  @fileoverview Text layout test commands.
  *  @author birm@rbirm.us (Ryan Birmingham)
  *  @license Copyright 2017 Ryan Birmingham.
  */
